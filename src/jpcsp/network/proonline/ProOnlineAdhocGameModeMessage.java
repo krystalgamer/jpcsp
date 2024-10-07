f2434005a75a0145c30aeacba59c493adcd3b1c0
krystalgamer <callofdutypsp@gmail.com>
2024-10-07 19:52:49 +0200

    checkpoint: new layout

--- src/jpcsp/network/proonline/ProOnlineAdhocGameModeMessage.java
+++
@@ -1,7 +0,0 @@
-package jpcsp.network.proonline;
-
-public class ProOnlineAdhocGameModeMessage extends ProOnlineAdhocMessage {
-	public ProOnlineAdhocGameModeMessage(ProOnlineNetworkAdapter networkAdapter, byte[] message, int length) {
-		super(networkAdapter, message, length);
-	}
-}
